c09d90222d0e9ac965cd85feafde69143ea403af
Tekano Khambane <tkhambane@gmail.com>
2021-08-07 11:57:20 +0200

    add TenantUser usertypes

--- users/models.py
+++ users/models.py
@@ -6,6 +6,9 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class TenantUser(UserProfile):
+    user_type_data = (('1', 'HOD'), ('2', 'Staff'), ('3', 'Customer'), ('4', 'AppUser'))
+    user_type = models.CharField(
+        default=1, choices=user_type_data, max_length=10)
     first_name = models.CharField(max_length=300, blank=True, null=True)
     last_name = models.CharField(max_length=300, blank=True, null=True)
     username = models.CharField(max_length=250, blank=True, null=True)
