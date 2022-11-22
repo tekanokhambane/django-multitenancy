16faeb16367f87d9b342e2d2515fbf978e16eac1
Tekano Khambane <tkhambane@gmail.com>
2022-11-22 16:10:25 +0200

    fix:change admin app to multitenancy

--- multitenancy/admin/apps.py
+++ multitenancy/admin/apps.py
@@ -1,9 +1,8 @@
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class MultiTenancyAdminConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'multitenancy.admin'
-    verbose_name = 'multitenancy_admin'
-    default = True
-    app_label = 'multitenancy_admin'
+    name = 'multitenancy'
+    verbose_name = _("Multitenancy admin")
+    app_label = 'multitenancyadmin'
