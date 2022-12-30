202920a7677c9290aa78068e05902c4831e81d8d
Tekano Khambane <tkhambane@gmail.com>
2022-12-30 21:43:02 +0200

    add dashboard stats on staff dashboard

--- multitenancy/admin/views/teamViews.py
+++ multitenancy/admin/views/teamViews.py
@@ -3,15 +3,24 @@ from django.views.generic import View
 from account.mixins import LoginRequiredMixin
 from multitenancy.admin.decorators import allowed_users
 from .baseViews import (StaffCreateView, StaffListView, TeamListView, StaffUpdateView)
+from helpdesk.models import Ticket
 
 
 class StaffIndexView(View, LoginRequiredMixin):
     @allowed_users(allowed_types=["Staff"])
     def get(self, request, *args, **kwargs):
+        active_tickets = Ticket.objects.select_related('queue').exclude(
+        status__in=[Ticket.CLOSED_STATUS, Ticket.RESOLVED_STATUS],
+        )
+
+        # open & reopened tickets, assigned to current user
+        tickets = active_tickets.filter(
+            assigned_to=request.user,
+        )
         return render(request, 'multitenancy/admin/teamUser/index.html',
                       {
                           'nbar': 'admin',
                           'title': 'Dashboard!',
-
+                        'user_tickets':tickets
                       }
                       )
