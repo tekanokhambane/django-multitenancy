41603d9a18cdf588553edfecf12b35b5ee8df6de
Tekano Khambane <tkhambane@gmail.com>
2023-01-23 01:14:05 +0200

    Add View for customer to create a service

--- multitenancy/subscriptions/urls.py
+++ multitenancy/subscriptions/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('multitenancy/plans', views.get_plans)
+    path('multitenancy/plans', views.get_plans),
+    path('multitenancy/plans/create', views.PlanView.as_view()),
 ]
--- multitenancy/subscriptions/views.py
+++ multitenancy/subscriptions/views.py
@@ -1,10 +1,26 @@
+import json
+from django.views.generic import View
+from django.http.response import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Plan
-from .serializers import PlanSerialiser
+from multitenancy.subscriptions.models import Plan, ProductType, Subscription
+from multitenancy.subscriptions.serializers import PlanSerialiser
 
 @api_view(["GET"])
 def get_plans(request):
     qs = Plan.objects.all()
     serializer = PlanSerialiser(qs, many=True)
     return Response(serializer.data)
+
+
+class PlanView(View):
+
+    def post(self, request):
+        data = json.loads(request.body)
+        duration = data.get('subscription_duration')
+        plan = data.get('plan')
+        
+        
+        # Do something with the selected plan and duration
+        # e.g. create a new subscription in the database
+        return JsonResponse({"plan": plan, "subscription_duration": duration})
