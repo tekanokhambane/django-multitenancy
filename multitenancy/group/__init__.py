9331cf0a079cf1a99d09e89060a2a81da0a135e7
Tekano Khambane <tkhambane@gmail.com>
2023-02-02 19:19:54 +0200

    remove groups apps

--- multitenancy/group/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'multitenancy.auth.apps.MultiTenancyAuthConfig'
--- multitenancy/group/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class GroupConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'multitenancy.group'
-    app_label = 'multitenancy_auth'
