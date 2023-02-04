61f17d5170d9de5d81935cc1147cc48fc1e0dff1
Tekano Khambane <tkhambane@gmail.com>
2023-02-04 20:31:04 +0200

    add order item to extended orders

--- multitenancy/order/models.py
+++ multitenancy/order/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.conf import settings
 from multitenancy.subscriptions.models import Subscription
+from multitenancy.address.models import Address
 # Create your models here.
 
 class Coupon(models.Model):
@@ -16,13 +18,17 @@ class Coupon(models.Model):
     # valid_for = models.ForeignKey(Subscription, on_delete=models.SET_NULL, null=True, blank=True) 
 
 class Order(models.Model):
-    subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,null=True, on_delete=models.CASCADE)
     date_created = models.DateField(auto_now_add=True)
     order_number = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=10, choices=[("completed", "Completed"), ("failed", "Failed")])
     payment_method = models.CharField(max_length=50)
-    shipping_address = models.TextField()
-    billing_address = models.TextField()
+    billing_address = models.ForeignKey(Address, on_delete=models.CASCADE)
     notes = models.TextField(blank=True)
     coupon = models.ForeignKey(Coupon, on_delete=models.SET_NULL, null=True, blank=True)
+
+
+class OrderItem(models.Model):
+    subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
