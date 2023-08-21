f423539f95318b56b1873f298aaf306a25cb7b8b
Tekano Khambane <tkhambane@gmail.com>
2023-08-21 16:40:06 +0200

    update serializer

--- multitenancy/apps/serializers.py
+++ multitenancy/apps/serializers.py
@@ -5,7 +5,7 @@ from .models import Tenant
 
 class TenantSerializer(serializers.ModelSerializer):
     subscription = SubscriptionSerializer(many=False)
-    plan = PlanSerialiser(many=False)
+    plan = PlanSerialiser(many=False, required=False)
     class Meta:
         model = Tenant
         fields = ['id', 'name', 'type', 'is_template', 'plan', 'description', 'subscription', 'trail_duration', 'on_trial', 'created', 'modified']
