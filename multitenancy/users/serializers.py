0c19281910b5addb04b6d6ce01375a043ee547c2
Tekano Khambane <tkhambane@gmail.com>
2023-08-23 09:37:58 +0200

    remove comments

--- multitenancy/users/serializers.py
+++ multitenancy/users/serializers.py
@@ -6,4 +6,4 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Staff
         fields = '__all__'
-        # fields = ['id', 'first_name', 'last_name', 'username', 'email', 'type','signup_confirmation', 'avatar', 'last_login']
+      
