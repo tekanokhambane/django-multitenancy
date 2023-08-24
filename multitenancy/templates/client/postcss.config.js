4cfcbbfab0301840e624ec52414c397a35adeb02
Tekano Khambane <tkhambane@gmail.com>
2023-08-24 09:24:28 +0200

    remove old template

--- multitenancy/templates/client/postcss.config.js
+++
@@ -1,8 +0,0 @@
-const tailwindcss = require('tailwindcss');
-module.exports = {
-    plugins: {
-        "postcss-preset-env": {},
-        'tailwindcss/nesting': {},
-        tailwindcss,
-    }
-}
