2f4c4bb764002de7e33d9ea86433c45723981baf
Tekano Khambane <tkhambane@gmail.com>
2023-09-26 12:46:26 +0200

    add Addres models

--- multitenancy/settings/models.py
+++ multitenancy/settings/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import RegexValidator
 from django.db import models
 from django.conf import settings
 from account.languages import DEFAULT_LANGUAGE
@@ -33,18 +34,41 @@ class GeneralInfo(BaseSetting):
     class Meta:
         verbose_name = 'General Details'
 
+class State(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class City(models.Model):
+    name = models.CharField(max_length=200)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
 
 class Address(BaseSetting):
     help_text = 'Update Company Address'
-    address_line_1 = models.CharField(max_length=200, null=True, blank=True,)
-    address_line_2 = models.CharField(max_length=200, null=True, blank=True)
-    city = models.CharField(max_length=200, null=True, blank=True)
-    state = models.CharField(max_length=200, null=True, blank=True, verbose_name="State/Province")
-    country = CountryField(null=True, blank=True)
-    postal_code = models.CharField(max_length=64, null=True, blank=True, verbose_name="Post/Zip-code")
+    address_line_1 = models.CharField(max_length=200, blank=True, help_text='Enter the first line of the address')
+    address_line_2 = models.CharField(max_length=200, blank=True,null=True, help_text='Enter the second line of the address')
+    city = models.CharField(max_length=200, blank=True, help_text='Enter the city')
+    state = models.ForeignKey(State, on_delete=models.CASCADE, null=True, blank=True, verbose_name="State/Province")
+    country = CountryField(blank=True, null=True,help_text='Select the country')
+    postal_code = models.CharField(
+        max_length=64,
+        blank=True,
+        verbose_name="Post/Zip-code",
+        validators=[RegexValidator(
+            regex=r'^\d{5}(?:[-\s]\d{4})?$',
+            message="Invalid postal code"
+        )],
+        help_text='Enter the postal or zip code'
+    )
 
     class Meta:
         verbose_name = 'Address'
+        ordering = ['id']
 
 
 class AdminSettings(BaseSetting):
@@ -56,16 +80,45 @@ class AdminSettings(BaseSetting):
     class Meta:
         verbose_name = 'Time zone and currency'
 
-class Currency(BaseSetting):
-    code = models.CharField(max_length=3)
-    name = models.CharField(max_length=50)
-    symbol = models.CharField(max_length=10)
-    default = models.BooleanField(default=False)
 
-    def save(self, *args, **kwargs):
-        if self.default:
-            Currency.objects.filter(default=True).update(default=False)
-        super().save(*args, **kwargs)
+class Currency(models.Model):
+    ACTIVE = 'active'
+    INACTIVE = 'inactive'
+    STATUS_CHOICES = [
+        (ACTIVE, 'Active'),
+        (INACTIVE, 'Inactive'),
+    ]
+    
+    code = models.CharField(max_length=3, unique=True, help_text="The currency code.")
+    name = models.CharField(max_length=50, help_text="The name of the currency.")
+    country = CountryField(help_text="The country of the currency.", null=True)
+    exchange_rate = models.DecimalField(max_digits=10, decimal_places=4, help_text="The exchange rate of the currency.", null=True)
+    created_at = models.DateTimeField(auto_now_add=True, help_text="The date and time the currency was created.",null=True)
+    updated_at = models.DateTimeField(auto_now=True, help_text="The date and time the currency was last updated.")
+    description = models.TextField(blank=True, null=True, help_text="Additional information about the currency.")
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=ACTIVE, help_text="Indicates if the currency is active or not.")
 
-    def __str__(self):
-        return self.code
+    class Meta:
+        verbose_name = "Currency"
+        verbose_name_plural = "Currencies"
+
+    def __str__(self) -> str:
+        """
+        Returns the currency code and name as a string.
+        """
+        return f'{self.code} - {self.name}'
+
+
+# class Currency(BaseSetting):
+#     code = models.CharField(max_length=3)
+#     name = models.CharField(max_length=50)
+#     symbol = models.CharField(max_length=10)
+#     default = models.BooleanField(default=False)
+
+#     def save(self, *args, **kwargs):
+#         if self.default:
+#             Currency.objects.filter(default=True).update(default=False)
+#         super().save(*args, **kwargs)
+
+#     def __str__(self):
+#         return self.code
