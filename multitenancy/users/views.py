453dfd6a391523c7dad42ea8a680cedb813cf71d
Tekano Khambane <tkhambane@gmail.com>
2024-04-16 19:42:26 +0200

    Refactor multitenancy user views for code style and efficiency improvements

--- multitenancy/users/views.py
+++ multitenancy/users/views.py
@@ -6,106 +6,114 @@ from django.urls.base import reverse, reverse_lazy
 from rest_framework.reverse import reverse
 from rest_framework import permissions
 from account.mixins import LoginRequiredMixin
-from multitenancy.admin.views.baseViews import(
+from multitenancy.admin.views.baseViews import (
     AdminDeleteView,
     AdminCreateView,
     AdminUpdateView,
     AdminTemplateView,
-    )
+)
 from tenant_users.permissions.models import UserTenantPermissions
 from .forms import CustomerForm, CustomerUpdateForm, StaffForm, StaffUpdateForm
 from .serializers import UserSerializer
 from .models import Customer, TenantUser, Staff
 
 
-class CustomerListView(LoginRequiredMixin ,AdminTemplateView):
+class CustomerListView(LoginRequiredMixin, AdminTemplateView):
     template_name = "multitenancy/users/customer_list.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        query = self.request.GET.get("q") 
+        query = self.request.GET.get("q")
         f = TenantUser.objects.filter(type="Customer").search(query=query)
-        context['filter'] = f
+        context["filter"] = f
         return context
 
-class DeleteCustomerView(LoginRequiredMixin ,AdminDeleteView):
+
+class DeleteCustomerView(LoginRequiredMixin, AdminDeleteView):
     model = Customer
     template_name = "multitenancy/users/delete_customer.html"
     success_url = reverse_lazy("customer_list", urlconf="multitenancy.urls")
 
     def delete(self, request, *args, **kwargs):
-        customer_id = self.kwargs['pk']
+        customer_id = self.kwargs["pk"]
 
         user = Customer.objects.filter(id=customer_id)
         user.delete()
-        return HttpResponseRedirect(reverse('customer_list', urlconf="multitenancy.urls"))
+        return HttpResponseRedirect(
+            reverse("customer_list", urlconf="multitenancy.urls")
+        )
 
-class CreateCustomerView(LoginRequiredMixin ,AdminCreateView):
+
+class CreateCustomerView(LoginRequiredMixin, AdminCreateView):
     model = TenantUser
     form_class = CustomerForm
     # success_url = '/admin/customers/'
-    success_url = reverse_lazy('customer_list', urlconf="multitenancy.urls")
-    template_name = 'multitenancy/users/create_customer.html'
+    success_url = reverse_lazy("customer_list", urlconf="multitenancy.urls")
+    template_name = "multitenancy/users/create_customer.html"
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
-        kwargs['initial'] = {'type': 'Customer'}
+        kwargs["initial"] = {"type": "Customer"}
         return kwargs
 
-class UpdateCustomerView(LoginRequiredMixin ,AdminUpdateView):
+
+class UpdateCustomerView(LoginRequiredMixin, AdminUpdateView):
     model = Customer
     form_class = CustomerUpdateForm
-    success_url = reverse_lazy('customer_list', urlconf="multitenancy.urls")
-    template_name = 'multitenancy/users/update_customer.html'
+    success_url = reverse_lazy("customer_list", urlconf="multitenancy.urls")
+    template_name = "multitenancy/users/update_customer.html"
 
 
-class StaffListView(LoginRequiredMixin ,AdminTemplateView):
+class StaffListView(LoginRequiredMixin, AdminTemplateView):
     template_name = "multitenancy/users/staff_list.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        query = self.request.GET.get("q") 
+        query = self.request.GET.get("q")
         f = TenantUser.objects.filter(type="Staff").search(query=query)
-        context['filter'] = f
+        context["filter"] = f
         return context
-    
 
-class CreateStaffView(LoginRequiredMixin ,AdminCreateView):
-    model = TenantUser
+
+class CreateStaffView(LoginRequiredMixin, AdminCreateView):
+    model = Staff
     form_class = StaffForm
     # success_url = '/admin/customers/'
-    success_url = reverse_lazy('staff_list', urlconf="multitenancy.urls")
-    template_name = 'multitenancy/users/create_staff.html'
+    success_url = reverse_lazy("staff_list", urlconf="multitenancy.urls")
+    template_name = "multitenancy/users/create_staff.html"
 
-    def get(self, request, *args, **kwargs):
-        form = self.form_class(initial={'type': 'Staff'})
-        return render(request, self.template_name, {'form': form})
-    
+    # def get(self, request, *args, **kwargs):
+    #     form = self.form_class(initial={"type": "Staff"})
+    #     return render(request, "multitenancy/users/create_staff.html", {"form": form})
+
     def form_valid(self, form):
         response = super().form_valid(form)
         user = form.instance
-        user_permission = UserTenantPermissions.objects.create(profile_id=user.pk, is_staff=True, is_superuser=False)
+        user_permission = UserTenantPermissions.objects.create(
+            profile_id=user.pk, is_staff=True, is_superuser=False
+        )
         user_permission.save()
         return response
 
-class UpdateStaffView(LoginRequiredMixin ,AdminUpdateView):
+
+class UpdateStaffView(LoginRequiredMixin, AdminUpdateView):
     model = Staff
     form_class = StaffUpdateForm
-    success_url = reverse_lazy('staff_list', urlconf="multitenancy.urls")
-    template_name = 'multitenancy/users/update_staff.html'
+    success_url = reverse_lazy("staff_list", urlconf="multitenancy.urls")
+    template_name = "multitenancy/users/update_staff.html"
 
 
-class DeleteStaffView(LoginRequiredMixin ,AdminDeleteView):
+class DeleteStaffView(LoginRequiredMixin, AdminDeleteView):
     model = Staff
     template_name = "multitenancy/users/delete_staff.html"
     success_url = reverse_lazy("staff_list", urlconf="multitenancy.urls")
 
     def delete(self, request, *args, **kwargs):
-        staff_id = self.kwargs['pk']
+        staff_id = self.kwargs["pk"]
 
         user = Staff.objects.filter(id=staff_id)
         user.delete()
-        return HttpResponseRedirect(reverse('staff_list', urlconf="multitenancy.urls"))
+        return HttpResponseRedirect(reverse("staff_list", urlconf="multitenancy.urls"))
 
 
 class CustomerViewSet(viewsets.ModelViewSet):
@@ -115,16 +123,22 @@ class CustomerViewSet(viewsets.ModelViewSet):
 
     Additionally we also provide an extra `highlight` action.
     """
+
     model = TenantUser
-    
+
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated,
-                          permissions.IsAdminUser]
+    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
 
     def get_queryset(self):
         query = self.request.GET.get("q")
-        id_query  = self.request.GET.get("id")
-        queryset = TenantUser.objects.filter(type="Customer").exclude(email="AnonymousUser").search(query=query).filter_by_id(query=id_query).distinct()
+        id_query = self.request.GET.get("id")
+        queryset = (
+            TenantUser.objects.filter(type="Customer")
+            .exclude(email="AnonymousUser")
+            .search(query=query)
+            .filter_by_id(query=id_query)
+            .distinct()
+        )
         return queryset
 
 
@@ -135,13 +149,13 @@ class StaffViewSet(viewsets.ModelViewSet):
 
     Additionally we also provide an extra `highlight` action.
     """
+
     model = TenantUser
-    
+
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated,
-                          permissions.IsAdminUser]
+    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
 
     def get_queryset(self):
-        query = self.request.GET.get("q") 
+        query = self.request.GET.get("q")
         queryset = TenantUser.objects.filter(type="Staff").search(query=query)
         return queryset
