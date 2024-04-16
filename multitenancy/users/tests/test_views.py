3b167877f40974a42e1f6fd84d3e6a0939cf8730
Tekano Khambane <tkhambane@gmail.com>
2024-04-16 14:34:29 +0200

    Uncomment and update tests for customer creation views in multitenancy app

--- multitenancy/users/tests/test_views.py
+++ multitenancy/users/tests/test_views.py
@@ -56,36 +56,36 @@ class CustomerViewsTestCase(unittest.TestCase):
         self.assertTrue(self.client.session.get("_auth_user_id"))
         self.assertIn("_auth_user_id", self.client.session)
 
+    def test_post_create_customer_view(self):
+        self.user = TenantUser.objects.create(
+            username="admin",
+            password="password",
+            first_name="abc123",
+            last_name="khamban",
+            email="abc123@email.com",
+            type="Admin",
+            is_active=True,
+        )
+        self.client.force_login(user=self.user)
+        request = self.factory.post("/admin/customers/create/", data=self.customer_data)
+        request.user = self.user
+        response = CreateCustomerView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(self.client.session.get("_auth_user_id"))
+        self.assertIn("_auth_user_id", self.client.session)
 
-#     def test_post_create_customer_view(self):
-#         self.user = TenantUser.objects.get(
-#             username='admin',
-#             password="password",
-#             first_name='abc123',
-#             last_name='khamban',
-#             email='abc123@email.com',
-#             type='Admin',
-#             is_active=True
-#             )
-#         self.client.force_login(user=self.user)
-#         request = self.factory.post('/admin/customers/create/', data=self.customer_data)
-#         request.user = self.user
-#         response = CreateCustomerView.as_view()(request)
-#         self.assertEqual(response.status_code, 200)
+    def test_get_create_customer_view_authenticated(self):
+        self.user = TenantUser.objects.get(
+            email="AnonymousUser",
+        )
 
-#     def test_get_create_customer_view_authenticated(self):
-#         self.user = TenantUser.objects.get(
+        self.client.force_login(user=self.user)
+        request = self.factory.get("/admin/customers/create/")
+        request.user = self.user
+        response = AdminIndexView.as_view()(request)
 
-#             email='AnonymousUser',
+        self.assertEqual(response.status_code, 404)
 
-#             )
-
-#         self.client.force_login(user=self.user)
-#         request = self.factory.get('/admin/customers/create/')
-#         request.user = self.user
-#         response = AdminIndexView.as_view()(request)
-
-#         self.assertEqual(response.status_code, 404)
 
 #     def test_update_customer_view(self):
 #         self.user = TenantUser.objects.get(
