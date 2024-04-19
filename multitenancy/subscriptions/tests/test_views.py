c2d78bfa8438e5798f1bcfa5123ffe77a8ee5f99
Tekano Khambane <tkhambane@gmail.com>
2024-04-19 21:35:18 +0200

    test: update plan viewset test

--- multitenancy/subscriptions/tests/test_views.py
+++ multitenancy/subscriptions/tests/test_views.py
@@ -53,18 +53,28 @@ from multitenancy.users.models import TenantUser
 class PlanViewsTestCase(unittest.TestCase):
 
     def setUp(self):
+        Plan.objects.all().delete()
         self.factory = RequestFactory()
         self.client = Client()
-
-    def test_get_create_plan_view(self):
-        self.user = Admin.objects.create(
+        #  self.user = Admin.objects.create(
+        #     username="admin",
+        #     password="password",
+        #     first_name="abc123",
+        #     last_name="khamban",
+        #     email="testaadminabc123@email.com",
+        #     is_active=True,
+        # )
+        self.user = TenantUser.objects.create_superuser(
             username="admin",
             password="password",
             first_name="abc123",
             last_name="khamban",
-            email="testaadminabc123@email.com",
+            email="testadminabc123@email.com",
+            type="Admin",
             is_active=True,
         )
+
+    def test_get_create_plan_view(self):
 
         self.client.force_login(user=self.user)
         request = self.factory.get("/admin/billing/plans/create/")
@@ -72,47 +82,38 @@ class PlanViewsTestCase(unittest.TestCase):
         response = CreatePlanView.as_view()(request)
         self.assertEqual(response.status_code, 200)
 
+    def test_post_create_plan_view(self):
 
-#     def test_post_create_plan_view(self):
-#         self.user = TenantUser.objects.create_superuser(
-#             username="admin",
-#             password="password",
-#             first_name="abc123",
-#             last_name="khamban",
-#             email="testadminabc123@email.com",
-#             type="Admin",
-#             is_active=True,
-#         )
-#         self.client.force_login(user=self.user)
-#         self.data = {
-#             "name": "basic",
-#             "description": "basic plan",
-#             "price": 78,
-#         }
-#         request = self.factory.post("/admin/plans/create/", data=self.data)
-#         request.user = self.user
-#         response = CreatePlanView.as_view()(request)
-#         self.assertEqual(response.status_code, 200)
+        self.client.force_login(user=self.user)
+        self.data = {
+            "name": "basic",
+            "description": "basic plan",
+            "price": 78,
+        }
+        request = self.factory.post("/admin/plans/create/", data=self.data)
+        request.user = self.user
+        response = CreatePlanView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
 
-#     def test_plan_detail_view(self):
-#         self.admin = TenantUser.objects.create_superuser(
-#             username="admin",
-#             password="password",
-#             first_name="abc123",
-#             last_name="khamban",
-#             email="test2adminabc123@email.com",
-#             type="Admin",
-#             is_active=True,
-#         )
-#         self.client.force_login(user=self.admin)
-#         self.plan = Plan.objects.create(
-#             name="enterprise",
-#         )
-#         self.request = self.factory.get(f"/billing/plans/{self.plan}/")
-#         self.request.user = self.admin
-#         view = PlanDetailView.as_view()
-#         response = view(self.request, pk=self.plan.pk)
-#         self.assertEqual(response.status_code, 200)
+    def test_plan_detail_view(self):
+        # self.admin = TenantUser.objects.create_superuser(
+        #     username="admin",
+        #     password="password",
+        #     first_name="abc123",
+        #     last_name="khamban",
+        #     email="test2adminabc123@email.com",
+        #     type="Admin",
+        #     is_active=True,
+        # )
+        self.client.force_login(user=self.user)
+        self.plan = Plan.objects.create(
+            name="enterprise",
+        )
+        self.request = self.factory.get(f"/billing/plans/{self.plan}/")
+        self.request.user = self.admin
+        view = PlanDetailView.as_view()
+        response = view(self.request, pk=self.plan.pk)
+        self.assertEqual(response.status_code, 200)
 
 
 # class TestPlanListView(unittest.TestCase):
