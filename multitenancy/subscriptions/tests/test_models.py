6987fb8a32ef041f17c9d1c11ce1b485a0606d69
Tekano Khambane <tkhambane@gmail.com>
2024-04-19 12:03:32 +0200

    test: update test_models.py

--- multitenancy/subscriptions/tests/test_models.py
+++ multitenancy/subscriptions/tests/test_models.py
@@ -491,9 +491,7 @@ class TestSubscription(unittest.TestCase):
         initial_renewal_date = subscribe.renewal_date
         # Check that the status, end date, and renewal date remain unchanged
         self.assertEqual(subscribe.status, initial_status)
-        self.assertEqual(
-            subscribe.end_date, initial_end_date + datetime.timedelta(days=30)
-        )
+        self.assertEqual(subscribe.end_date, initial_end_date)
         self.assertEqual(subscribe.renewal_date, initial_renewal_date)
 
     # Test that the duration of an expired subscription can be updated successfully
