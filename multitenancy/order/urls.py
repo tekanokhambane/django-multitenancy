eff49f870832ec7f1f16dded6af1a978a4b01a0e
Tekano Khambane <tkhambane@gmail.com>
2024-04-29 17:08:13 +0200

    create coupon view

--- multitenancy/order/urls.py
+++ multitenancy/order/urls.py
@@ -3,7 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('billing/orders/',views.OrdersListView.as_view(), name="orders_list"),
-    path('billing/coupons/',views.CouponListView.as_view(), name="coupon_list"),
-    path('billing/orders/dashboard/', views.OrderIndexView.as_view(), name="orders_index" ),
+    path("billing/orders/", views.OrdersListView.as_view(), name="orders_list"),
+    path("billing/coupons/", views.CouponListView.as_view(), name="coupon_list"),
+    path(
+        "billing/coupons/create/",
+        views.CreateCouponView.as_view(),
+        name="create_coupon",
+    ),
+    path(
+        "billing/orders/dashboard/", views.OrderIndexView.as_view(), name="orders_index"
+    ),
 ]
--- multitenancy/order/views.py
+++ multitenancy/order/views.py
@@ -1,16 +1,21 @@
+from django.forms import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
 from account.mixins import LoginRequiredMixin
-from multitenancy.admin.views.baseViews import AdminTemplateView
+from django.urls import reverse_lazy
+from multitenancy.admin.views.baseViews import AdminTemplateView, AdminCreateView
 from rest_framework import viewsets
 
 from multitenancy.order.models import Order, Coupon
 from multitenancy.order.serializers import CouponSerializer, OrdersSerializer
+from multitenancy.order.form import CouponForm
 
 
 class OdersViewSet(viewsets.ModelViewSet):
     model = Order
     serializer_class = OrdersSerializer
     queryset = Order.objects.all()
+
 
 class CouponViewSet(viewsets.ModelViewSet):
     model = Coupon
@@ -22,11 +27,28 @@ class OrderIndexView(AdminTemplateView):
     template_name = "multitenancy/order/order_index.html"
 
 
-class OrdersListView(LoginRequiredMixin ,AdminTemplateView):
+class OrdersListView(LoginRequiredMixin, AdminTemplateView):
 
-    template_name = 'multitenancy/order/orders_list.html'
+    template_name = "multitenancy/order/orders_list.html"
 
 
-class CouponListView(LoginRequiredMixin ,AdminTemplateView):
+class CouponListView(LoginRequiredMixin, AdminTemplateView):
 
-    template_name = 'multitenancy/order/coupon_list.html'
+    template_name = "multitenancy/order/coupon_list.html"
+
+
+class CreateCouponView(LoginRequiredMixin, AdminCreateView):
+    model = Coupon
+    form_class = CouponForm
+    success_url = reverse_lazy("coupon_list", urlconf="multitenancy.urls")
+    template_name = "multitenancy/order/create_coupon.html"
+
+    # print logs
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        print(form.errors)
+        return super().form_valid(form)
+
+    def form_invalid(self, form: BaseModelForm) -> HttpResponse:
+        print(form.errors)
+        return super().form_invalid(form)
